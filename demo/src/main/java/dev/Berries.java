d37fe46d53bfb0efd653b6c9433f427ad8ae256e
Tolli-txt <tolli.txt@gmail.com>
2023-01-26 00:00:09 +0100

    Added new Berries class

---
+++ demo/src/main/java/dev/Berries.java
@@ -0,0 +1,15 @@
+package dev;
+
+class Berries extends Fruit {
+    // public Berries() {
+    //     setName(getName());
+    //     setPrice(getPrice());
+    //     setQuantity(getQuantity());
+    // }
+
+    public Berries(String name, double price, int quantity) {
+        setName(name);
+        setPrice(price);
+        setQuantity(quantity);
+    }    
+}
