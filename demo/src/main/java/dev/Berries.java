43bb910fcf089ddac6afe00c5825feb8d14893db
Tolli-txt <tolli.txt@gmail.com>
2023-01-26 21:38:18 +0100

    Added Berries subclass that extends Fruit.java class in a more DRY way

--- demo/src/main/java/dev/Berries.java
+++ demo/src/main/java/dev/Berries.java
@@ -1,12 +1,9 @@
 package dev;
 
 class Berries extends Fruit {
-    // public Berries() {
-    //     setName(getName());
-    //     setPrice(getPrice());
-    //     setQuantity(getQuantity());
-    // }
-
+    // Extends the fruit class, variables name, price, quantity are used
+    // so that you can make multiple berries without needing to make more classes
+    // in other words, only one class is needed when doing it like this
     public Berries(String name, double price, int quantity) {
         setName(name);
         setPrice(price);
